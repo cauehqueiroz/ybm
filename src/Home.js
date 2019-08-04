4854191bc8b146e3f8b93907be5bf069cc9d9326
Caueh <caueh.queiroz@gmail.com>
2019-08-04 11:58:54 -0300

    Order by date completed

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
 
+    bannerMovies = [];
+
     constructor() {
         super();
-        
         this.state = {
             sortByDate: false,
             showType: 'movie', // movie or series
             movies: [
                 {
+                    "Position": 1,
                     "Title": "Spider-Man: Far from Home",
                     "Year": "2019",
                     "imdbID": "tt6320628",
@@ -20,6 +22,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 2,
                     "Title": "Avengers: Endgame",
                     "Year": "2019",
                     "imdbID": "tt4154796",
@@ -28,6 +31,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 3,
                     "Title": "Star Wars: Episode IV - A New Hope",
                     "Year": "1977",
                     "imdbID": "tt0076759",
@@ -36,6 +40,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 4,
                     "Title": "John Wick",
                     "Year": "2014",
                     "imdbID": "tt2911666",
@@ -44,6 +49,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 5,
                     "Title": "Toy Story",
                     "Year": "1995",
                     "imdbID": "tt0114709",
@@ -52,6 +58,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 6,
                     "Title": "The Lord of the Rings: The Fellowship of the Ring",
                     "Year": "2001",
                     "imdbID": "tt0120737",
@@ -60,6 +67,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 7,
                     "Title": "The Lord of the Rings: The Return of the King",
                     "Year": "2003",
                     "imdbID": "tt0167260",
@@ -68,6 +76,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 8,
                     "Title": "The Lord of the Rings: The Two Towers",
                     "Year": "2002",
                     "imdbID": "tt0167261",
@@ -76,6 +85,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 9,
                     "Title": "The Simpsons",
                     "Year": "1989",
                     "imdbID": "tt0096697",
@@ -84,6 +94,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 10,
                     "Title": "Spider-Man: Into the Spider-Verse",
                     "Year": "2018",
                     "imdbID": "tt4633694",
@@ -92,6 +103,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 11,
                     "Title": "The Office",
                     "Year": "2005",
                     "imdbID": "tt0386676",
@@ -100,6 +112,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMTgzNjAzMDE0NF5BMl5BanBnXkFtZTcwNTEyMzM3OA@@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 12,
                     "Title": "How I Met Your Mother",
                     "Year": "2005",
                     "imdbID": "tt0460649",
@@ -108,6 +121,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BZWJjMDEzZjUtYWE1Yy00M2ZiLThlMmItODljNTAzODFiMzc2XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 13,
                     "Title": "The Flash",
                     "Year": "2014",
                     "imdbID": "tt3107288",
@@ -116,6 +130,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMjI1MDAwNDM4OV5BMl5BanBnXkFtZTgwNjUwNDIxNjM@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 14,
                     "Title": "Dark",
                     "Year": "2017",
                     "imdbID": "tt5753856",
@@ -124,6 +139,7 @@ class Home extends React.Component {
                     "Poster": "https://m.media-amazon.com/images/M/MV5BMmIyZjA3NGUtYjlhNS00ZDlkLWI0MDgtMDc2YWNjNGMwYWZhXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_SX300.jpg"
                 },
                 {
+                    "Position": 15,
                     "Title": "Daredevil",
                     "Year": "2015",
                     "imdbID": "tt3322312",
@@ -138,6 +154,16 @@ class Home extends React.Component {
         this.handleChangeSortByDate = this.handleChangeSortByDate.bind(this);
         this.handleClickFavorite = this.handleClickFavorite.bind(this);
         this.getList = this.getList.bind(this);
+
+        this.bannerMovies = [];
+        let topMovie = this.getTop('movie');
+        if(topMovie) {
+            this.bannerMovies.push(topMovie);
+        }
+        let topSeries = this.getTop('series');
+        if(topSeries) {
+            this.bannerMovies.push(topSeries);
+        }
     }
 
     componentDidMount() {
@@ -162,25 +188,16 @@ class Home extends React.Component {
         let topMovieUrl = "/details/" + movie.imdbID;
         return (
             <li key={movie.imdbID} className="list-group-item">
-                <Link to={topMovieUrl}>#{ (i+1) } - {movie.Title}</Link>
+                <Link to={topMovieUrl}>#{ movie.Position } - {movie.Title}</Link> ({ movie.Year})
                 <i className={"fa fa-heart ml-3 " + (movie.Favorite ? 'text-danger' : 'text-secondary')} onClick={(e) => this.handleClickFavorite(movie)}></i>
             </li>
         );
     }
 
     render() {
-        let bannerMovies = [];
-        let topMovie = this.getTop('movie');
-        if(topMovie) {
-            bannerMovies.push(topMovie);
-        }
-        let topSeries = this.getTop('series');
-        if(topSeries) {
-            bannerMovies.push(topSeries);
-        }
-        
+                
         let banner = "";
-        if(bannerMovies.length > 0) {
+        if(this.bannerMovies.length > 0) {
             banner = (
                 <section className="jumbotron text-center bg-dark">
                     <div className="container">
@@ -190,7 +207,7 @@ class Home extends React.Component {
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         </ol>
                         <div className="carousel-inner">
-                            { bannerMovies.map(this.getBannerMovie)}
+                            { this.bannerMovies.map(this.getBannerMovie)}
                         </div>
                         <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                             <span className="carousel-control-prev-icon text-primary" aria-hidden="true"></span>
@@ -206,7 +223,8 @@ class Home extends React.Component {
             );
         }
 
-        let MovieList = this.state.movies.filter(movie => movie.Type === this.state.showType).map(this.getList);
+        let MovieList = this.state.sortByDate ? this.getMovieOrderByDate(this.state.movies) : this.getMovieOrderByPosition(this.state.movies);
+        MovieList = MovieList.filter(movie => movie.Type === this.state.showType).map(this.getList);
 
         return (
         <div>
@@ -254,10 +272,41 @@ class Home extends React.Component {
                     { MovieList}
                     </ul>
                 </div>
+                <div className="col">
+                    
+                </div>
             </div>
         </section>
         </div>
         )
+    }
+
+    getMovieOrderByDate(movies) {
+        movies.sort(function (a, b) {
+            if (a.Year > b.Year) {
+              return 1;
+            }
+            if (a.Year < b.Year) {
+              return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+        return movies;
+    }
+
+    getMovieOrderByPosition(movies) {
+        movies.sort(function (a, b) {
+            if (a.Position > b.Position) {
+              return 1;
+            }
+            if (a.Position < b.Position) {
+              return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+        return movies;
     }
 
     handleClickFavorite(movieClicked) {
